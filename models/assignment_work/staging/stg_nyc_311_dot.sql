40853f70fcaeb99bea72a66bd59fa70a50d6ea60
Angela L <xylin100@gmail.com>
2026-03-19 03:47:07 +0000

    adjust date/time adjustments

--- models/assignment_work/staging/stg_nyc_311_dot.sql
+++ models/assignment_work/staging/stg_nyc_311_dot.sql
@@ -33,8 +33,8 @@ cleaned AS (
        CAST(unique_key AS STRING) AS request_id,
 
        -- Date/Time
-       CAST(created_date AS TIMESTAMP) AS created_date,
-       CAST(closed_date AS TIMESTAMP) AS closed_date,
+       --CAST(created_date AS TIMESTAMP) AS created_date,
+       --CAST(closed_date AS TIMESTAMP) AS closed_date,
 
        -- Request details
        CAST(agency AS STRING) AS agency,
