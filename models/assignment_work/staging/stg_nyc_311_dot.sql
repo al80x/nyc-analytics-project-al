e4be2bf79aa0e70c7283645b4c1c65076c4f4a8d
Angela L <xylin100@gmail.com>
2026-03-28 03:06:53 +0000

    test time

--- models/assignment_work/staging/stg_nyc_311_dot.sql
+++ models/assignment_work/staging/stg_nyc_311_dot.sql
@@ -33,7 +33,6 @@ cleaned AS (
        CAST(unique_key AS STRING) AS request_id,
 
        -- Date/Time
-       FORMAT_DATETIME("%Y-%m-%d %H:%M:%S", created_date) AS created_date,
        CAST(created_date AS TIMESTAMP) AS created_date,
        --CAST(closed_date AS TIMESTAMP) AS closed_date,
 
